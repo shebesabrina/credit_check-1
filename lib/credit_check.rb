6d5d5df664108ac1f422803df9fed3ceaa1956c1
Sabrina Robinson <sabrinarob@gmail.com>
2018-03-12 18:59:25 -0600

    Adding method to double every other integer.

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class CreditCheck
   attr_reader :card
   def initialize(card)
@@ -6,8 +8,20 @@ class CreditCheck
 
   def times_2
     reverse = @card.digits
-    reverse.map.with_index do |number, index|
+    digit_index = reverse.map.with_index do |number, index|
       number * 2 if index.odd?
     end
+    digit_index.compact
   end
+
+  def double_digit
+    times_2.map do |number|
+      if number > 9
+        number - 9
+      else
+        number
+      end
+    end
+  end
+
 end
--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -12,7 +12,13 @@ class CreditCheckTest < Minitest::Test
   end
 
   def test_it_can_multiple_other_digit_from_right_most
-    assert_equal 0, @credit_check.times_2
+    result = [8, 0, 4, 14, 10, 14, 4, 8]
+    assert_equal result, @credit_check.times_2
+  end
+
+  def test_it_can_add_any_double_digits_in_array
+    result = [8, 0, 4, 5, 1, 5, 4, 8]
+    assert_equal result, @credit_check.double_digit
   end
 
 end
